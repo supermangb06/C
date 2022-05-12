9b61ec52f68262cfb7bb3396070a50b9bdcb3bfd
supermangb06 <supermangb06@yahoo.com>
2022-05-12 12:22:53 -0400

    ch 10 calendar code

---
+++ ch 10 mini calendar functions.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool is_leap_year(int year)
+{
+if (year % 4 == 0){
+  return true;}
+  else if (year % 100 == 0){
+    return true;
+  }
+  else if (year % 400 != 0 && year % 100 == 0){
+    return false;
+  }
+  else{
+  return false;}
+  return}
+
+int main() {
+  int year;
+  printf("Please enter a year between 1800-10000"); 
+  scanf("%d", &year);
+  is_leap_year(year); 
+  if (is_leap_year = true){
+    printf("This is a leap year!");}
+    else
+    {printf("Not a leap year");
+    }
+  }
+
